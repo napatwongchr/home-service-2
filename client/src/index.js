db24358b67cec610d5862f9e7897b5d55601920b
Touchsung <jettapat.th@gmail.com>
2022-10-31 19:05:05 +0700

    feat:setup theme

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from './theme.js';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
