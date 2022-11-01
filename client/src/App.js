459716baee5d2f69d5e19dd4d86a75ee96f70c9e
Touchsung <jettapat.th@gmail.com>
2022-11-01 16:37:27 +0700

    feat setup register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
-import ServiceList from "./pages/ServiceList";
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RegisterPage from './pages/RegisterPage'
+import HomePage from "./pages/HomePage";
 function App() {
   return (
-    <div className="App">
-      <ServiceList />
 
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </Router>
   );
 }
 
