83034f12293ab547c8f880465f2eb76b86ce1e7a
Touchsung <jettapat.th@gmail.com>
2022-11-01 10:29:34 +0700

    feat: add recommend service section component

---
+++ client/src/asset/model/serviceData.js
@@ -0,0 +1,56 @@
+export const serviceData = [
+    {
+        'name': 'ล้างแอร์',
+        'price': '500.00 - 1,000.00',
+        'type': 'บริการทั่วไป',
+        'image': require(`../image/serviceImage.js/ล้างแอร์.png`)
+    },
+    {
+        'name': 'ติดตั้งแอร์',
+        'price': '2,800.00',
+        'type': 'บริการทั่วไป',
+        'image': require(`../image/serviceImage.js/ติดตั้งแอร์.png`)
+    },
+    {
+        'name': 'ซ่อมแอร์',
+        'price': '400.00',
+        'type': 'บริการทั่วไป',
+        'image': require(`../image/serviceImage.js/ซ่อมแอร์.png`)
+    },
+    {
+        'name': 'ทำความสะอาดทั่วไป',
+        'price': '500.00',
+        'type': 'บริการทั่วไป',
+        'image': require(`../image/serviceImage.js/ทำความสะอาดทั่วไป.png`)
+    },
+    {
+        'name': 'ซ่อมเครื่องซักผ้า',
+        'price': '500.00',
+        'type': 'บริการทั่วไป',
+        'image': require(`../image/serviceImage.js/ซ่อมเครื่องซักผ้า.png`)
+    },
+    {
+        'name': 'ติดตั้งเตาแก๊ส',
+        'price': '1, 000.00',
+        'type': 'บริการห้องครัว',
+        'image': require(`../image/serviceImage.js/ติดตั้งเตาแก๊ส.png`)
+    },
+    {
+        'name': 'ติดตั้งเครื่องดูดควัน',
+        'price': '1,000.00',
+        'type': 'บริการห้องครัว ',
+        'image': require(`../image/serviceImage.js/ติดตั้งเครื่องดูดควัน.png`)
+    },
+    {
+        'name': 'ติดตั้งชักโครก',
+        'price': '1,000.00',
+        'type': 'บริการห้องน้ำ',
+        'image': require(`../image/serviceImage.js/ติดตั้งชักโครก.png`)
+    },
+    {
+        'name': 'ติดตั้งเครื่องทำน้ำอุ่น',
+        'price': '500.00',
+        'type': 'บริการห้องน้ำ',
+        'image': require(`../image/serviceImage.js/ติดตั้งเครื่องทำน้ำอุ่น.png`)
+    },
+]
