303b2479fdcfb36e0cbc61df0e568b3e272efb2c
naisu <napas14204@gmaill.com>
2022-11-01 23:46:26 +0700

    style: fix css

--- client/src/components/search-component.js
+++ client/src/components/search-component.js
@@ -31,8 +31,19 @@ const SearchSection = () => {
   const [alphabetSearch, setAlphabetSearch] = "";
 
   return (
-    <Container className="search-bar" maxW="100%" height="84px" border="1px">
-      <Flex justify="space-evenly" height="100%" align="center">
+    <Container
+      className="search-bar"
+      maxW="100%"
+      height="84px"
+      border="1px"
+      borderColor="gray.100"
+    >
+      <Stack
+        direction="row"
+        justify="space-evenly"
+        alignItems="center"
+        h="100%"
+      >
         <Box className="search-input">
           <Flex align="center">
             <InputGroup>
@@ -56,91 +67,122 @@ const SearchSection = () => {
           </Flex>
         </Box>
 
-        <Stack
-          className="filter-section"
-          direction="row"
-          align="center"
-          height="100%"
-        >
-          <Box className="category" padding="1rem">
-            <Text>หมวดหมู่บริการ</Text>
-            <Select
-              border="none"
-              icon={<Image src={arrow} maxWidth="10px" />}
-              value={category}
-              onChange={(e) => setCategory(e.target.value)}
+        <Box className="filter-section">
+          <Flex alignItems="center">
+            <Box className="category" mr="20px">
+              <Text fontSize="12px" ml="15px" color="gray.700">
+                หมวดหมู่บริการ
+              </Text>
+              <Select
+                border="none"
+                textStyle="h5"
+                height="22px"
+                icon={<Image src={arrow} maxWidth="10px" />}
+                value={category}
+                _hover={{
+                  background: "#EFEFF2",
+                }}
+              >
+                <option value="general-services">บริการทั้งหมด</option>
+                <option value="general-services">บริการทั่วไป</option>
+                <option value="kitchen-services">บริการห้องครัว</option>
+                <option value="restrooom-services">บริการห้องน้ำ</option>
+              </Select>
+            </Box>
+
+            <Box
+              className="price"
+              borderLeft="1px"
+              borderColor="gray.300"
+              alignItems="center"
+              pl="10px"
+              mr="20px"
             >
-              <option default value="general-services">
-                บริการทั้งหมด
-              </option>
-              <option default value="general-services">
-                บริการทั่วไป
-              </option>
-              <option value="kitchen-services">บริการห้องครัว</option>
-              <option value="restrooom-services">บริการห้องน้ำ</option>
-            </Select>
-          </Box>
-          <hr />
-          <Box className="price" width="180px" alignItems="center">
-            <Menu>
-              <Text>ราคา</Text>
-              <MenuButton
-                as={Button}
-                rightIcon={<Image src={arrow} />}
-                width="180px"
-                textAlign="left"
-                bg="white"
-                _hover
+              <Menu>
+                <Text fontSize="12px" ml="15px" color="gray.700">
+                  ราคา
+                </Text>
+                <MenuButton
+                  as={Button}
+                  rightIcon={<Image src={arrow} />}
+                  textAlign="left"
+                  height="22px"
+                  bg="white"
+                  w="150px"
+                  _hover={{
+                    background: "#EFEFF2",
+                  }}
+                >
+                  0-2000฿
+                </MenuButton>
+                <MenuList
+                  height="100px"
+                  width="250px"
+                  padding="10px"
+                  display="flex"
+                  alignItems="center"
+                >
+                  <MenuItem>
+                    <RangeSlider
+                      defaultValue={[0, 240]}
+                      min={0}
+                      max={2000}
+                      step={30}
+                      onChangeEnd={(val) => setSliderValue(val)}
+                      display="flex"
+                      justifyContent="space-between"
+                    >
+                      <RangeSliderTrack bg="#CCD0D7">
+                        <RangeSliderFilledTrack bg="blue.500" />
+                      </RangeSliderTrack>
+                      <RangeSliderThumb boxSize={4} index={0} bg="blue.700" />
+                      <Text marginTop="40px" color="blue.700">
+                        {sliderValue[0]}
+                      </Text>
+                      <RangeSliderThumb boxSize={4} index={1} bg="blue.700" />
+                      <Text marginTop="40px" color="blue.700">
+                        {sliderValue[1]}
+                      </Text>
+                    </RangeSlider>
+                  </MenuItem>
+                </MenuList>
+              </Menu>
+            </Box>
+
+            <Box
+              className="sort-by"
+              borderLeft="1px"
+              borderColor="gray.300"
+              alignItems="center"
+              pl="10px"
+              w="250px"
+              mr="10px"
+            >
+              <Text fontSize="12px" ml="15px" color="gray.700">
+                เรียงตาม
+              </Text>
+              <Select
+                border="none"
+                textStyle="h5"
+                height="22px"
+                icon={<Image src={arrow} maxWidth="10px" />}
+                value={alphabetSearch}
+                _hover={{
+                  background: "#EFEFF2",
+                }}
               >
-                0-2000฿
-              </MenuButton>
-              <MenuList
-                height="100px"
-                width="250px"
-                padding="10px"
-                display="flex"
-                alignItems="center"
-              >
-                <MenuItem>
-                  <RangeSlider
-                    defaultValue={[0, 240]}
-                    min={0}
-                    max={2000}
-                    step={30}
-                    onChangeEnd={(val) => setSliderValue(val)}
-                    display="flex"
-                    justifyContent="space-between"
-                  >
-                    <RangeSliderTrack bg="#CCD0D7">
-                      <RangeSliderFilledTrack bg="blue.500" />
-                    </RangeSliderTrack>
-                    <RangeSliderThumb boxSize={4} index={0} bg="blue.700" />
-                    <Text marginTop="40px" color="blue.700">
-                      {sliderValue[0]}
-                    </Text>
-                    <RangeSliderThumb boxSize={4} index={1} bg="blue.700" />
-                    <Text marginTop="40px" color="blue.700">
-                      {sliderValue[1]}
-                    </Text>
-                  </RangeSlider>
-                </MenuItem>
-              </MenuList>
-            </Menu>
-          </Box>
-          <Box className="sort-by">
-            <Text>เรียงตาม </Text>
-            <Select border="none" icon={<Image src={arrow} maxWidth="10px" />}>
-              <option value="recommended-services">บริการแนะนำ</option>
-              <option value="popular-services">บริการยอดนิยม</option>
-              <option value="ascending">ตามตัวอักษร (Ascending)</option>
-              <option value="descending">ตามตัวอักษร (Descending)</option>
-            </Select>
-          </Box>
-          <Button bg="blue.600" textColor="white" textStyle="h5">
-            ค้นหา
-          </Button>
-        </Stack>
-      </Flex>
+                <option value="recommended-services">บริการแนะนำ</option>
+                <option value="popular-services">บริการยอดนิยม</option>
+                <option value="ascending">ตามตัวอักษร (Ascending)</option>
+                <option value="descending">ตามตัวอักษร (Descending)</option>
+              </Select>
+            </Box>
+            <Button bg="blue.600" textColor="white" textStyle="h5" w="5rem">
+              ค้นหา
+            </Button>
+          </Flex>
+        </Box>
+      </Stack>
     </Container>
   );
 };
