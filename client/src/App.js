6b177ac4b3fc5a80cdcb09732826d876d977c3e6
naisu <napas14204@gmaill.com>
2022-11-02 16:16:25 +0700

    style: UI login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <LoginPage />
     </div>
   );
 }
 
 export default App;
-
---
+++ client/src/pages/LoginPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "../components/HomePageComponent/Nav";
+import LoginForm from "../components/LoginPage/LoginForm";
+
+const RegisterPage = () => {
+  return (
+    <section className="registerPage">
+      <Nav />
+      <LoginForm />
+    </section>
+  );
+};
+
+export default RegisterPage;
