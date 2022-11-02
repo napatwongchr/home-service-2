850c4d70b6def21fd8c2cfa2cedf651005e8bd5a
naisu <napas14204@gmaill.com>
2022-11-02 23:59:45 +0700

    feat: css with auth login

---
+++ client/src/utils/jwtInterceptor.js
@@ -0,0 +1,18 @@
+import axios from "../api/axios";
+
+function jwtInterceptor() {
+  axios.interceptors.request.use((req) => {
+    const hasToken = Boolean(window.localStorage.getItem("token"));
+
+    if (hasToken) {
+      req.headers = {
+        ...req.headers,
+        Authorization: `Bearer ${window.localStorage.getItem("token")}`,
+      };
+    }
+
+    return req;
+  });
+}
+
+export default jwtInterceptor;
