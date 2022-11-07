f55be0bcf8be7fe6c461f8995af940440983c54d
Touchsung <jettapat.th@gmail.com>
2022-11-07 18:37:36 +0700

    feat: update create service list page

--- client/src/components/AdminServiceList/CreateServiceCategory.js
+++ client/src/components/AdminServiceList/CreateServiceCategory.js
@@ -1,10 +1,21 @@
-import { Button, Container, Flex, FormLabel, Image, Input, Menu, MenuButton, MenuItem, MenuList, Text } from "@chakra-ui/react";
+import { Button, Container, Flex, FormLabel, Image, Input, Menu, MenuButton, MenuItem, MenuList, Text, } from "@chakra-ui/react";
 import { useState } from "react";
 import arrow from "../../asset/image/serviceListPage/dropdown.svg";
-import imageIcon from '../../asset/image/adminServiceList/imageIcon.svg'
+import imageIcon from '../../asset/image/adminServiceList/imageIcon.svg';
+import plusIcon from '../../asset/image/adminDashboardPage/plusIcon.svg';
+import { Formik, Form, FieldArray } from 'formik';
+import { MyFieldInput } from '../../utils/formInput'
 const CreateServiceList = () => {
   const [category, setCategory] = useState('เลือกหมวดหมู่');
-
+  const initialValues = {
+    serviceList: [
+      {
+        name: '',
+        price: '',
+        unit: ''
+      },
+    ],
+  };
   return (
     <Container maxW='100%' p='40px' h='calc(100% - 3.25rem)' bg='gray.100'>
       <Flex bg='utility.white' px='24px' py='40px' border='1px' borderColor='gray.200' borderRadius='8px' flexDirection='column' gap='40px'>
@@ -113,18 +124,106 @@ const CreateServiceList = () => {
             w={'440px'} h={'180px'}
             border='1px dashed'
             pos='relative'
-            display='none' />
+            display='none'
+            accept="image/png, image/jpeg" />
           <FormLabel pos='relative' display='flex' flexDirection='column' gap='12px' alignItems='center' w={'440px'} h={'180px'}
             border='1px dashed' borderColor='gray.300' textStyle='b3' color='gray.700' py='35px'>
 
             <Image src={imageIcon} w='36px' />
-            <Text display='flex'><FormLabel m='0' mr='6px' for='serviceImage' color='blue.600' cursor={'pointer'} >อัพโหลดรูปภาพ</FormLabel>หรือ ลากและวางที่นี่</Text>
+            <Text display='flex'><FormLabel m='0' mr='6px' htmlFor='serviceImage' color='blue.600' cursor={'pointer'} >อัพโหลดรูปภาพ</FormLabel>หรือ ลากและวางที่นี่</Text>
             <Text>PNG, JPG ขนาดไม่เกิน 5MB</Text>
             <Text color={'gray.700'} textStyle='b3' fontSize='12px' pos='absolute' bottom={-8} left={0} >ขนาดภาพที่แนะนำ: 1440 x 225 PX</Text>
           </FormLabel>
         </Flex>
         <hr />
-        <Text color={'gray.700'} textStyle={'h5'}>รายการบริการย่อย</Text>
+        <Text color={'gray.700'} textStyle={'h5'} mb='-30px'>รายการบริการย่อย</Text>
+        <Formik
+          initialValues={initialValues}
+          onSubmit={async (values) => {
+            await new Promise((r) => setTimeout(r, 500));
+            alert(JSON.stringify(values, null, 2));
+          }}
+        >
+          {({ values }) => (
+            <Form>
+              <FieldArray name='serviceList'>
+                {({ insert, remove, push }) => (
+                  <div>
+                    {values.serviceList.map((item, index) => (
+                      <Flex gap='10px' alignItems={'end'} key={index}>
+                        {/* <Flex flexDirection={'column'}>
+                          <FormLabel htmlFor={`serviceList.${index}.name`}>ชื่อรายการ</FormLabel>
+                          <Field as={Input} name={`serviceList.${index}.name`} type="text" />
+                        </Flex> */}
+                        <MyFieldInput
+                          label="ชื่อรายการ"
+                          id={`serviceList.${index}.name`}
+                          name={`serviceList.${index}.name`}
+                          type="text"
+                          w={'440px'} h={'44px'} mt='0'
+                        />
+                        <MyFieldInput
+                          label="ค่าบริการ / 1 หน่วย"
+                          id={`serviceList.${index}.price`}
+                          name={`serviceList.${index}.price`}
+                          type="text"
+                          w={'240px'} h={'44px'} mt='0'
+                        />
+                        <MyFieldInput
+                          label="หน่วยการบริการ"
+                          id={`serviceList.${index}.unit`}
+                          name={`serviceList.${index}.unit`}
+                          type="text"
+                          w={'240px'} h={'44px'} mt='0'
+                        />
+                        <Button variant={'ghost'} color='gray.400' onClick={() => values.serviceList.length > 1 && remove(index)}>ลบรายการ</Button>
+                      </Flex>
+                    ))}
+                    <Button variant={'secondary'} rightIcon={<Image src={plusIcon} />} mt='40px' px='25px'
+                      onClick={() => push({
+                        name: '',
+                        price: '',
+                        unit: ''
+                      })}>
+                      เพิ่มรายการ
+                    </Button>
+                  </div>
+                )}
+              </FieldArray>
+            </Form>
+          )}
+
+        </Formik>
+
+        {/* <Form> */}
+
+        {/* <Flex gap='10px' alignItems={'end'}>
+              <MyTextInput
+                label="ชื่อรายการ"
+                id="serviceList"
+                name="serviceList"
+                type="text"
+                w={'440px'} h={'44px'} mt='0'
+              />
+              <MyTextInput
+                label="ค่าบริการ / 1 หน่วย"
+                id="price"
+                name="price"
+                type="text"
+                w={'240px'} h={'44px'} mt='0'
+              />
+              <MyTextInput
+                label="หน่วยการบริการ"
+                id="unit"
+                name="unit"
+                type="text"
+                w={'240px'} h={'44px'} mt='0'
+              />
+              <Button variant={'ghost'} color='gray.400'>ลบรายการ</Button>
+            </Flex>
+            <Button variant={'secondary'} rightIcon={<Image src={plusIcon} />} mt='40px' px='25px'>เพิ่มรายการ</Button> */}
+        {/* </Form> */}
+
       </Flex >
     </Container >
   );
