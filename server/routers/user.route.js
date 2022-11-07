07cc1b7674c889171cb67f3d18f8c0d27158ea94
Touchsung <jettapat.th@gmail.com>
2022-11-07 12:06:38 +0700

    fix: endpoint to check email

--- server/routers/user.route.js
+++ server/routers/user.route.js
@@ -4,7 +4,6 @@ import userController from '../controllers/user.controller.js'
 const userRoute = Route();
 
 userRoute.get('/', userController.getUser)
-userRoute.get('/email', userController.getUserByEmail)
 userRoute.post('/register', userController.register)
 userRoute.post('/login', userController.login)
 
