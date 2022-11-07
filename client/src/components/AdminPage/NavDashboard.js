065e9fb1324c2238830fea6517c77e74160f8a90
Touchsung <jettapat.th@gmail.com>
2022-11-07 13:03:34 +0700

    feat: updated nav of admin page

--- client/src/components/AdminPage/NavDashboard.js
+++ client/src/components/AdminPage/NavDashboard.js
@@ -1,20 +1,18 @@
-import { Button, Container, Flex, Input, Text } from "@chakra-ui/react";
+import { Button, Flex, Img, Input, Text } from "@chakra-ui/react";
 import React from "react";
-
-const NavDashBoard = () => {
+import searchIcon from '../../asset/image/adminDashboardPage/searchIcon.svg'
+const NavDashBoard = (props) => {
 
     return (
-        <Container w='1200px' h='80px' bg='blue.950' py='32px' px='0' display='flex' justifyContent='space-between' m={0}>
-            <Flex>
-                <Text>หมวดหมู่</Text>
-                <Flex>
-                    <Input>
+        <Flex w='1200px' h='80px' bg='utility.white' borderBottom='1px' borderColor='gray.100' justifyContent='space-between' alignItems='center' px='40px'>
+            <Text textStyle='h2' color='utility.black'>{props.children}</Text>
+            < Flex pos='relative' gap='24px' >
+                <Img src={searchIcon} pos='absolute' left='19px' top='13px' />
+                <Input borderRadius='8px' placeholder={`ค้นหา${props.children}...`} w='350px' h='44px' pl='50px' />
+                <Button w='165px' h='44px'>เพิ่ม{props.children}</Button>
+            </ Flex >
 
-                    </Input>
-                    <Button> เพิ่มหมวดหมู่</Button>
-                </Flex>
-            </Flex>
-        </Container>
+        </Flex >
     )
 }
 
--- client/src/pages/Admin/AdminDashBoard.js
+++ client/src/pages/Admin/AdminDashBoard.js
@@ -1,4 +1,4 @@
-import { Container, Flex } from '@chakra-ui/react';
+import { Container, Flex, Box } from '@chakra-ui/react';
 import React from 'react';
 import NavDashBoard from '../../components/AdminPage/NavDashboard';
 import SideBar from '../../components/AdminPage/SideBar';
@@ -6,9 +6,16 @@ import SideBar from '../../components/AdminPage/SideBar';
 
 const adminDashboardPage = () => {
     return (
-        <Container className='adminDashboardPage' display='flex' w='1920px' m='0' p='0'>
-            <SideBar />
-            <NavDashBoard />
+        <Container className='adminDashboardPage' centerContent>
+            <Flex>
+                <SideBar />
+                <Box>
+
+                    <NavDashBoard>
+                        หมวดหมู่
+                    </NavDashBoard>
+                </Box>
+            </Flex>
         </Container >
 
     )
