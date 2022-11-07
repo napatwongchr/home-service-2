53ecc94391fe71839ec02f6db1a6209a8fb65575
naisu <napas14204@gmaill.com>
2022-11-07 12:42:52 +0700

    feat: service category hardcode ver.

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,17 @@ import RegisterPage from "./pages/RegisterPage";
 import HomePage from "./pages/HomePage";
 import ServiceList from "./pages/ServiceList";
 import LoginPage from "./pages/LoginPage";
+import ViewServiceCategory from "./components/ServiceCategory/ViewServiceCategory";
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/service-list" element={<ServiceList />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/login" element={<LoginPage />} />
-    </Routes>
+    // <Routes>
+    //   <Route path="/" element={<HomePage />} />
+    //   <Route path="/service-list" element={<ServiceList />} />
+    //   <Route path="/register" element={<RegisterPage />} />
+    //   <Route path="/login" element={<LoginPage />} />
+    // </Routes>
+    <ViewServiceCategory />
   );
 }
 
