e3cf5ace5b140b792ea0ed2d4975853637edd382
Touchsung <jettapat.th@gmail.com>
2022-11-08 12:16:12 +0700

    feat: update file uploading to beckend

--- server/routers/service.route.js
+++ server/routers/service.route.js
@@ -1,11 +1,18 @@
 import Route from 'express'
 import serviceCategoryController from '../controllers/service-category.contoller.js';
+import serviceListController from '../controllers/service-list.controller.js';
+import multer from "multer";
 
+const upload = multer({ dest: 'uploads/' });
 const serviceRoute = Route();
 
+// Category
 serviceRoute.get('/category', serviceCategoryController.getServiceCategory);
 serviceRoute.post('/category', serviceCategoryController.createServiceCategory);
 serviceRoute.put('/category', serviceCategoryController.editServiceCategory);
 serviceRoute.delete('/category', serviceCategoryController.deleteServiceCategory);
+
+
+serviceRoute.post('/category/list', upload.single('serviceImage'), serviceListController.createServiceList);
 
 export default serviceRoute;
