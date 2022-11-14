de6cbb5150f9338ab0f1028526d0bf7b737ec076
naisu <napas14204@gmaill.com>
2022-11-14 10:47:10 +0700

    resolved conflict

--- client/src/components/ServiceCategory/ViewServiceCategory.js
+++ client/src/components/ServiceCategory/ViewServiceCategory.js
@@ -1,7 +1,9 @@
 import binIcon from "../../asset/image/serviceCategory/bin-icon.svg";
 import editIcon from "../../asset/image/serviceCategory/edit-icon.svg";
 import warningICon from "../../asset/image/serviceCategory/warning-icon.svg";
-import { useNavigate } from "react-router-dom";
+import searchIcon from "../../asset/image/adminDashboardPage/searchIcon.svg";
+
+import { useNavigate, Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 import {
@@ -9,6 +11,7 @@ import {
   Flex,
   Image,
   Table,
+  Input,
   Thead,
   Tbody,
   Tr,
@@ -29,159 +32,203 @@ import useServiceCategories from "../../hooks/useServiceCategories.js";
 
 const ViewServiceCategory = () => {
   const navigate = useNavigate();
-  const {
-    serviceCategories,
-    getServiceCategories,
-    params,
-    deleteServiceCategory,
-  } = useServiceCategories();
-  const [categorySearch, setCategorySearch] = useState("");
+  const { serviceCategories, getServiceCategories, deleteServiceCategory } =
+    useServiceCategories();
+
   const [categoryName, setCategoryName] = useState("");
   const [categoryUniqueId, setCategoryUniqueId] = useState();
   const { isOpen, onOpen, onClose } = useDisclosure();
 
+  const [searchCategoryName, setSearchCategoryName] = useState("");
+
   useEffect(() => {
-    getServiceCategories();
-  }, [serviceCategories]);
+    getServiceCategories({ searchCategoryName });
+  }, [serviceCategories, searchCategoryName]);
 
   return (
-    <Container maxW="100%" p="40px" h="calc(100% - 3.25rem)" bg="gray.100">
-      <TableContainer
-        maxW="100%"
-        border={"1px"}
-        borderColor="gray.200"
-        borderRadius="10px"
-        bg="utility.white"
-      >
-        <Table variant="simple">
-          <Thead>
-            <Tr bg={"gray.100"}>
-              <Th fontWeight="400" width="10px" fontSize={"14px"}>
-                ลำดับ
-              </Th>
-              <Th fontWeight="400" fontSize={"14px"}>
-                ชื่อหมวดหมู่
-              </Th>
-              <Th fontWeight="400" fontSize={"14px"}>
-                สร้างเมื่อ
-              </Th>
-              <Th fontWeight="400" fontSize={"14px"}>
-                แก้ไขล่าสุด
-              </Th>
-              <Th
-                fontWeight="400"
-                textAlign={"center"}
-                textTransform="none"
-                fontSize={"14px"}
-              >
-                Action
-              </Th>
-            </Tr>
-          </Thead>
-          <Tbody className="categoty-box">
-            {serviceCategories.map((item, index) => {
-              return (
-                <Tr
-                  className="categoty-lists"
-                  height={"88"}
-                  key={item.service_category_id}
+    <Container maxW="100%" height="100%" bg="gray.100" padding={"0"}>
+      <Flex direction="column" alignItems={"center"} maxW="100vw" h="100%">
+        <Flex
+          h="80px"
+          w="100%"
+          bg="utility.white"
+          borderBottom="1px"
+          borderColor="gray.100"
+          justifyContent="space-between"
+          alignItems="center"
+          px="40px"
+        >
+          <Text textStyle="h2" color="utility.black">
+            หมวดหมู่
+          </Text>
+          <Flex pos="relative" gap="24px">
+            <Image
+              src={searchIcon}
+              pos="absolute"
+              left="19px"
+              top="13px"
+              zIndex={2}
+            />
+            <Input
+              borderRadius="8px"
+              placeholder={`ค้นหาหมวดหมู่...`}
+              w="350px"
+              h="44px"
+              pl="50px"
+              value={searchCategoryName}
+              onChange={(e) => {
+                setSearchCategoryName(e.target.value);
+              }}
+            />
+
+            <Link to="/admin-dashboard/category/create">
+              <Button w="165px" h="44px">
+                เพิ่มหมวดหมู่
+              </Button>
+            </Link>
+          </Flex>
+        </Flex>
+
+        <TableContainer
+          width="85%"
+          border={"1px"}
+          borderColor="gray.200"
+          borderRadius="10px"
+          bg="utility.white"
+          marginTop="2rem"
+        >
+          <Table variant="simple">
+            <Thead>
+              <Tr bg={"gray.100"}>
+                <Th fontWeight="400" width="10px" fontSize={"14px"}>
+                  ลำดับ
+                </Th>
+                <Th fontWeight="400" fontSize={"14px"}>
+                  ชื่อหมวดหมู่
+                </Th>
+                <Th fontWeight="400" fontSize={"14px"}>
+                  สร้างเมื่อ
+                </Th>
+                <Th fontWeight="400" fontSize={"14px"}>
+                  แก้ไขล่าสุด
+                </Th>
+                <Th
+                  fontWeight="400"
+                  textAlign={"center"}
+                  textTransform="none"
+                  fontSize={"14px"}
                 >
-                  <Td className="sequence" textAlign={"center"}>
-                    {index + 1}
-                  </Td>
-                  <Td className="category-name">
-                    <button
-                      onClick={() => {
-                        navigate(
-                          `/admin-dashboard/category/view/${item.service_category_id}`
-                        );
-                      }}
-                    >
-                      {item.service_category_name}
-                    </button>
-                  </Td>
-                  <Td className="created-at">{item.created_at}</Td>
-                  <Td className="edited-at">{item.updated_at}</Td>
-                  <Td className="action-button">
-                    <Flex justifyContent={"space-evenly"}>
+                  Action
+                </Th>
+              </Tr>
+            </Thead>
+            <Tbody className="categoty-box">
+              {serviceCategories.map((item, index) => {
+                return (
+                  <Tr
+                    className="categoty-lists"
+                    height={"88"}
+                    key={item.service_category_id}
+                  >
+                    <Td className="sequence" textAlign={"center"}>
+                      {index + 1}
+                    </Td>
+                    <Td className="category-name">
                       <button
-                        className="delete-button"
-                        onClick={() => {
-                          setCategoryName(`${item.service_category_name}`);
-                          setCategoryUniqueId(`${item.service_category_id}  `);
-                          onOpen();
-                        }}
-                      >
-                        <Image src={binIcon} alt="bin icon" />
-                        <Modal isOpen={isOpen} onClose={onClose}>
-                          <ModalOverlay />
-                          <ModalContent
-                            textAlign="center"
-                            height="250px"
-                            width="fit-content"
-                            borderRadius={"16px"}
-                          >
-                            <ModalHeader marginTop="1.5rem">
-                              <Flex direction="column" alignItems={"center"}>
-                                <Image
-                                  src={warningICon}
-                                  alt="warning icon"
-                                  width="30px"
-                                  marginBottom="10px"
-                                />
-                                <Text textStyle={"h2"} color="gray.950">
-                                  ยืนยันการลบรายการ?
-                                </Text>
-                              </Flex>
-                            </ModalHeader>
-                            <ModalBody maxH="30px" paddingTop="-15px">
-                              <Text fontWeight={300}>
-                                คุณต้องการลบรายการ '{categoryName}' ใช่หรือไม่
-                              </Text>
-                            </ModalBody>
-                            <ModalFooter alignSelf={"center"}>
-                              <Button
-                                onClick={() => {
-                                  deleteServiceCategory(categoryUniqueId);
-                                  onClose();
-                                }}
-                                colorScheme="blue"
-                                mr={3}
-                              >
-                                ลบรายการ
-                              </Button>
-                              <Button
-                                onClick={onClose}
-                                variant="ghost"
-                                color="blue.600"
-                                border={"1px"}
-                                borderColor={"blue.600"}
-                                textDecoration="none"
-                              >
-                                ยกเลิก
-                              </Button>
-                            </ModalFooter>
-                          </ModalContent>
-                        </Modal>
-                      </button>
-                      <button
-                        className="edit-button"
                         onClick={() => {
                           navigate(
-                            `/admin-dashboard/category/edit/${item.service_category_id}`
+                            `/admin-dashboard/category/view/${item.service_category_id}`
                           );
                         }}
                       >
-                        <Image src={editIcon} alt="edit icon" />
+                        {item.service_category_name}
                       </button>
-                    </Flex>
-                  </Td>
-                </Tr>
-              );
-            })}
-          </Tbody>
-        </Table>
-      </TableContainer>
+                    </Td>
+                    <Td className="created-at">{item.created_at}</Td>
+                    <Td className="edited-at">{item.updated_at}</Td>
+                    <Td className="action-button">
+                      <Flex justifyContent={"space-evenly"}>
+                        <button
+                          className="delete-button"
+                          onClick={() => {
+                            setCategoryName(`${item.service_category_name}`);
+                            setCategoryUniqueId(
+                              `${item.service_category_id}  `
+                            );
+                            onOpen();
+                          }}
+                        >
+                          <Image src={binIcon} alt="bin icon" />
+                          <Modal isOpen={isOpen} onClose={onClose}>
+                            <ModalOverlay />
+                            <ModalContent
+                              textAlign="center"
+                              height="250px"
+                              width="fit-content"
+                              borderRadius={"16px"}
+                            >
+                              <ModalHeader marginTop="1.5rem">
+                                <Flex direction="column" alignItems={"center"}>
+                                  <Image
+                                    src={warningICon}
+                                    alt="warning icon"
+                                    width="30px"
+                                    marginBottom="10px"
+                                  />
+                                  <Text textStyle={"h2"} color="gray.950">
+                                    ยืนยันการลบรายการ?
+                                  </Text>
+                                </Flex>
+                              </ModalHeader>
+                              <ModalBody maxH="30px" paddingTop="-15px">
+                                <Text fontWeight={300}>
+                                  คุณต้องการลบรายการ '{categoryName}' ใช่หรือไม่
+                                </Text>
+                              </ModalBody>
+                              <ModalFooter alignSelf={"center"}>
+                                <Button
+                                  onClick={() => {
+                                    deleteServiceCategory(categoryUniqueId);
+                                    onClose();
+                                  }}
+                                  colorScheme="blue"
+                                  mr={3}
+                                >
+                                  ลบรายการ
+                                </Button>
+                                <Button
+                                  onClick={onClose}
+                                  variant="ghost"
+                                  color="blue.600"
+                                  border={"1px"}
+                                  borderColor={"blue.600"}
+                                  textDecoration="none"
+                                >
+                                  ยกเลิก
+                                </Button>
+                              </ModalFooter>
+                            </ModalContent>
+                          </Modal>
+                        </button>
+                        <button
+                          className="edit-button"
+                          onClick={() => {
+                            navigate(
+                              `/admin-dashboard/category/edit/${item.service_category_id}`
+                            );
+                          }}
+                        >
+                          <Image src={editIcon} alt="edit icon" />
+                        </button>
+                      </Flex>
+                    </Td>
+                  </Tr>
+                );
+              })}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Flex>
     </Container>
   );
 };
--- server/controllers/service-category.contoller.js
+++ server/controllers/service-category.contoller.js
@@ -2,110 +2,129 @@ import { query } from "express";
 import { pool } from "../utils/db.js";
 
 const serviceCategoryController = {
-    async createServiceCategory(req, res) {
-        try {
-            const findCategory = await pool.query(`select * from service_category where service_category_name = $1`, [req.body.categoryName])
-            const hasCategory = Boolean(findCategory.rows[0])
+  async createServiceCategory(req, res) {
+    try {
+      const findCategory = await pool.query(
+        `select * from service_category where service_category_name = $1`,
+        [req.body.categoryName]
+      );
+      const hasCategory = Boolean(findCategory.rows[0]);
 
-            if (hasCategory) {
-                return res.status(403).json({
-                    msg: "this service has already exist"
-                })
-            }
-            const serviceCategory = await pool.query(`insert into service_category( service_category_name, created_at, updated_at )
+      if (hasCategory) {
+        return res.status(403).json({
+          msg: "this service has already exist",
+        });
+      }
+      const serviceCategory = await pool.query(
+        `insert into service_category( service_category_name, created_at, updated_at )
             values (
                 $1,
                 to_char(current_timestamp, 'DD/MM/YYYY HH:MI AM'),
                 to_char(current_timestamp, 'DD/MM/YYYY HH:MI AM')
             ) returning service_category_id
-            `, [req.body.categoryName])
+            `,
+        [req.body.categoryName]
+      );
 
-            return res.status(201).json({
-                msg: "add service successfully",
-                data: serviceCategory.rows[0]
-            })
+      return res.status(201).json({
+        msg: "add service successfully",
+        data: serviceCategory.rows[0],
+      });
+    } catch (err) {
+      return res.status(400).json({
+        msg: "invalid input",
+      });
+    }
+  },
 
-        } catch (err) {
-            return res.status(400).json({
-                msg: "invalid input"
-            })
+  async getServiceCategory(req, res) {
+    try {
+      //find category by categoryId
+      if (req.query.categoryId) {
+        let categoryId = req.query.categoryId;
+        const findCategory = await pool.query(
+          `select * from service_category where service_category_id = $1`,
+          [categoryId]
+        );
+        if (!Boolean(findCategory.rows[0])) {
+          return res.status(404).json({
+            msg: "category not found",
+          });
         }
-    },
-
-    async getServiceCategory(req, res) {
-        try {
-            //find category by categoryId
-            if (req.query.categoryId) {
-                let categoryId = req.query.categoryId
-                const findCategory = await pool.query(`select * from service_category where service_category_id = $1`, [categoryId])
-                if (!Boolean(findCategory.rows[0])) {
-                    return res.status(404).json({
-                        msg: "category not found"
-                    })
-                }
-                return res.status(200).json({
-                    data: findCategory.rows[0]
-                })
-            }
-            else if (req.query.categoryName) {
-                let categoryName = req.query.categoryName
-                const findCategory = await pool.query(`select * from service_category where service_category_name = $1`, [categoryName])
-                if (!Boolean(findCategory.rows[0])) {
-                    return res.status(404).json({
-                        msg: "category not found"
-                    })
-                }
-                return res.status(200).json({
-                    data: findCategory.rows[0]
-                })
-            }
-            else if (Object.keys(req.query).length > 0) {
-                return res.status(404).json({
-                    msg: "invalid query"
-                })
-            }
-
-            const allServiceCategory = await pool.query(`select * from service_category`)
-            return res.status(200).json({
-                data: allServiceCategory.rows
-            })
-        } catch (err) {
-            return res.status(400).json({
-                msg: "invalid input"
-            })
+        return res.status(200).json({
+          data: findCategory.rows[0],
+        });
+      } else if (req.query.categoryName) {
+        let categoryName = req.query.categoryName;
+        const findCategory = await pool.query(
+          `select * from service_category where service_category_name = $1`,
+          [categoryName]
+        );
+        if (!Boolean(findCategory.rows[0])) {
+          return res.status(404).json({
+            msg: "category not found",
+          });
         }
-    },
+        return res.status(200).json({
+          data: findCategory.rows[0],
+        });
+      } else if (Object.keys(req.query).length > 0) {
+        return res.status(404).json({
+          msg: "invalid query",
+        });
+      }
 
-    async editServiceCategory(req, res) {
-        try {
-            let categoryId = req.query.categoryId
-            let categoryName = req.body.categoryName
-            await pool.query(`update service_category
+      const allServiceCategory = await pool.query(
+        `select * from service_category`
+      );
+      return res.status(200).json({
+        data: allServiceCategory.rows,
+      });
+    } catch (err) {
+      return res.status(400).json({
+        msg: "invalid input",
+      });
+    }
+  },
+
+  async editServiceCategory(req, res) {
+    try {
+      let categoryId = req.query.categoryId;
+      let categoryName = req.body.categoryName;
+      await pool.query(
+        `update service_category
                 set service_category_name = $1, 
                 updated_at = to_char(current_timestamp, 'DD/MM/YYYY HH:MI AM')
                 where service_category_id = $2
-            `, [categoryName, categoryId])
+            `,
+        [categoryName, categoryId]
+      );
 
-            const getRecentupdate = await pool.query(`select * from service_category where service_category_id = $1`, [categoryId])
+      const getRecentupdate = await pool.query(
+        `select * from service_category where service_category_id = $1`,
+        [categoryId]
+      );
 
-            return res.status(200).json({
-                msg: "category updated",
-                data: getRecentupdate.rows[0]
-            })
-
-        } catch (err) {
-            return res.status(400).json({
-                msg: "invalid input"
-            })
-        }
-    },
-
-    async deleteServiceCategory(req, res) {
-        await pool.query(`delete from service_category where service_category_id = $1`, [req.query.categoryId])
-        return res.status(202).json({
-            msg: "category has been deleted"
-        })
+      return res.status(200).json({
+        msg: "category updated",
+        data: getRecentupdate.rows[0],
+      });
+    } catch (err) {
+      return res.status(400).json({
+        msg: "invalid input",
+      });
     }
-}
+  },
+
+  async deleteServiceCategory(req, res) {
+    await pool.query(
+      `delete from service_category where service_category_id = $1`,
+      [req.query.categoryId]
+    );
+    return res.status(202).json({
+      msg: "category has been deleted",
+    });
+  },
+};
 
 export default serviceCategoryController;
