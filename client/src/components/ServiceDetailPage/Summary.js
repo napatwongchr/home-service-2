937aa75e58763abfbee329f85bba3addea8035e1
Suger13 <muteki.scarlette@gmail.com>
2022-11-22 19:03:59 +0700

    add summary component

---
+++ client/src/components/ServiceDetailPage/Summary.js
@@ -0,0 +1,39 @@
+import { Flex, Text, Box } from "@chakra-ui/react"
+
+const Summary = (props) =>{
+
+    const { subService, setSubService, serviceList } = props
+    
+    return (
+        <>
+            <Flex bgColor="utility.white" h="400px" borderRadius="8px" p="2rem" direction="column">
+                <Text textColor="gray.700" textStyle="h3">สรุปรายการ</Text>
+
+                {/* AddOnList */}
+                <Flex borderBottom="1px" pb="1rem" direction="column" mt="1rem">
+                    {subService.map((subService, index) => {
+                        return(
+                            <Flex key={index} gap="0.5rem">
+                                <Text textColor="utility.black">{subService.sub_service_name}</Text>
+                                <Text textColor="gray.900" textStyle="h4">{subService.count} รายการ</Text>
+                            </Flex>
+                        )
+                    })}
+                    
+                    {/* Total */}
+                    <Flex justifyContent="space-between">
+                        <Text textColor="gray.700" >รวม</Text>
+                        <Text textColor="utility.black" fontWeight="600">{subService.reduse( (acc, cur) =>{
+
+                        } )}฿</Text>
+                    </Flex>
+
+                </Flex>
+
+                
+            </Flex>
+        </>
+    )
+}
+
+export default Summary;
