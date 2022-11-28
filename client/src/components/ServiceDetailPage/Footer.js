16f44f2adb280b3b82ce06a71d960ad52ff66f69
Pun Svetachinta <pun_svt@Puns-MacBook-Air.local>
2022-11-28 17:07:24 +0700

    feat: create handlesubmit not success

--- client/src/components/ServiceDetailPage/Footer.js
+++ client/src/components/ServiceDetailPage/Footer.js
@@ -1,6 +1,7 @@
 import { Button, Container, Flex } from "@chakra-ui/react";
 import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
 import { useEffect, useState } from "react";
+import axios from "../../api/axios";
 const Footer = (props) => {
   const {
     setPage,
@@ -10,18 +11,21 @@ const Footer = (props) => {
     pickTime,
     homeAddress,
     summaryAddress,
-    setStatus
+    cardNumber,
+    cardName,
+    cardExp,
+    cardCVC,
+    setStatus,
   } = props;
   const [disable, setDisable] = useState(true);
   useEffect(() => {
+    // if (page === 3 && cardNumber && cardName && cardExp && cardCVC) {
+    //   setDisable(false);
+    // } else {
+    //   setDisable(true);
+    // }
     subService.map((item) => {
-      if (
-        page === 2 &&
-        pickDate &&
-        pickTime &&
-        homeAddress &&
-        summaryAddress
-      ) {
+      if (page === 2 && pickDate && pickTime && homeAddress && summaryAddress) {
         if (
           summaryAddress.district === "" ||
           summaryAddress.province === "" ||
@@ -37,7 +41,48 @@ const Footer = (props) => {
         setDisable(true);
       }
     });
-  }, [subService, pickDate, pickTime, homeAddress, summaryAddress]);
+  }, [
+    subService,
+    pickDate,
+    pickTime,
+    homeAddress,
+    summaryAddress,
+    cardNumber,
+    cardName,
+    cardExp,
+    cardCVC,
+  ]);
+
+  const jsonData = {
+    // userId :  user id (number),
+    // serviceId : service id (number),
+    appointmentDate: pickDate,
+    appointmentTime: pickTime,
+    address: homeAddress + summaryAddress,
+    totalPrice: subService.sub_total_price,
+    subService: [
+      {
+        sub_service_id: subService.sub_service_id,
+        // quantity : quantity (number),
+        sub_total_price:
+          subService.length > 1
+            ? subService.reduce((acc, cur) => {
+                return acc + cur.sub_total_price;
+              }, 0)
+            : subService.map((subService) => subService.sub_total_price),
+      },
+      {},
+    ],
+  };
+
+  console.log(jsonData);
+  const handleSubmit = async (jsonData) => {
+    try {
+      await axios.post("/orders", JSON.stringify(jsonData));
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <Container
@@ -81,10 +126,11 @@ const Footer = (props) => {
             if (page < 3) {
               setPage(page + 1);
               setDisable(true);
-            }else{
-              setStatus(true)
+            } else {
+              setStatus(true);
             }
           }}
+          onSubmit={handleSubmit}
         >
           {props.children}
         </Button>
